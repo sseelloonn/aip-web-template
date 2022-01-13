d4c0ac55247d3a2e1ea9382813826a2ecfbe3347
tufpsj <tufpsj@yonyou.com>
2022-01-13 21:02:20 +0800

    # 修改菜单

--- src/Menus.tsx
+++ src/Menus.tsx
@@ -1,31 +1,11 @@
 import { LinkItem } from "amis/lib/components/AsideNav";
 import FrameComp from "./pages/FrameComp";
-import JsonSchema from "./pages/JsonSchema";
-import JsonDebug from "./pages/JsonDebug";
 
-export function PersionBlog() {
-  return <FrameComp title="个人博客" src="https://www.tufeiping.cn" />;
+export function BuildTabFrame(url: string, title?: string) {
+  return ()=>{
+  return <FrameComp title={title || '新增页面'} src={url} />;
+  }
 }
-
-export function YonyouAud() {
-  return <FrameComp title="用友审计" src="https://www.yonyouaud.com" />;
-}
-
-export function JSONSchema() {
-  return <JsonSchema />;
-}
-
-export function JsonDebugger() {
-  return <JsonDebug/>
-}
-
-export function Overview() {
-  let currentPath = window.location.protocol + "//" + window.location.host + window.location.pathname;
-  return (
-    <FrameComp title="单独页面" src={currentPath + "iframe.html#/overview"} />
-  );
-}
-
 
 const menus: Array<LinkItem> = [
   {
@@ -33,27 +13,12 @@ const menus: Array<LinkItem> = [
     children: [
       {
         label: "用友审计",
-        path: "/#/yonyouaud",
+        path: "https://www.yonyouaud.com",
         children: [],
       },
       {
         label: "个人网站",
-        path: "/#/personblog",
-        children: [],
-      }
-    ],
-  },
-  {
-    label: "JSON组件测试",
-    children: [
-      {
-        label: "JSON组件测试",
-        path: "/#/json",
-        children: [],
-      },
-      {
-        label: "JSON组件调试",
-        path: "/#/jsondebug",
+        path: "https://www.tufeiping.cn",
         children: [],
       }
     ],
@@ -63,7 +28,7 @@ const menus: Array<LinkItem> = [
     children: [
       {
         label: "JSON组件测试",
-        path: "http://localhost:3000/iframe.html#/overview",
+        path: "./iframe.html#/overview",
         children: [],
       },
     ],
@@ -80,24 +45,16 @@ export interface NavRouterItem {
  */
 export const linkRouters: Array<NavRouterItem> = [
   {
-    path: "/personblog",
-    component: PersionBlog,
+    path: "https://www.tufeiping.cn",
+    component: BuildTabFrame("https://www.tufeiping.cn", "个人网站"),
   },
   {
-    path: "/yonyouaud",
-    component: YonyouAud,
+    path: "https://www.yonyouaud.com",
+    component: BuildTabFrame("https://www.yonyouaud.com", "用友审计"),
   },
   {
-    path: "/json",
-    component: JSONSchema,
-  },
-  {
-    path: "/jsondebug",
-    component: JsonDebugger,
-  },
-  {
-    path: "/overview",
-    component: Overview,
+    path: "./iframe.html#/overview",
+    component: BuildTabFrame("./iframe.html#/overview", "JSON组件测试"),
   }
 ];
 
